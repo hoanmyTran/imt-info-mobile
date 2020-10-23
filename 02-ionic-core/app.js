36b15107d3ea2817ee3c15fb92313a63d4784015
hoanmyTran <hoanmytran.hmt@gmail.com>
2020-10-23 09:24:55 +0200

    added blog's data

--- 02-ionic-core/app.js
+++ 02-ionic-core/app.js
@@ -1,18 +1,15 @@
-/*const API_URL = `https://devfest-nantes-2018-api.cleverapps.io/blog`;
-const IMAGES_URL = `https://devfest2018.gdgnantes.com`;
-
-console.log("Hello");
+const API_URL = `https://devfest-nantes-2018-api.cleverapps.io/blog`;
+const IMAGE_URL = `https://devfest2018.gdgnantes.com`;
 
 fetch(API_URL)
     .then(res => res.json())
     .then(elements => 
         {
-            console.log("hello");
             let list ="";
             for (let i in elements) {
                 let element = elements[i];
                 console.log(element);
+                list += "<ion-card> <img src=\""+IMAGE_URL+element.image+"\" /> <ion-card-header> <ion-card-title>"+element.title+"</ion-card-title></ion-card-header> <ion-card-content>"+element.brief+"</ion-card-content> </ion-card>";
             }
+            $("#blog").html(list);
         });
-        */
-console.log('haha');
