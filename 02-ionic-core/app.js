418410f71e4eade6c8d04c912c96d8a26037ee45
hoanmyTran <hoanmytran.hmt@gmail.com>
2020-10-23 09:00:14 +0200

    ionic content changed

--- 02-ionic-core/app.js
+++ 02-ionic-core/app.js
@@ -1,4 +1,7 @@
-fetch(`https://devfest-nantes-2018-api.cleverapps.io/blog`)
+const API_URL = `https://devfest-nantes-2018-api.cleverapps.io/blog`;
+const IMAGES_URL = `https://devfest2018.gdgnantes.com`;
+
+fetch(API_URL)
     .then(res => res.json())
     .then(posts => 
         {
@@ -6,8 +9,3 @@ fetch(`https://devfest-nantes-2018-api.cleverapps.io/blog`)
             console.log(posts)
             childToPrint.src = posts[0].title;
         });
-
-/*hello = async function() {
-    alert('Hello World!');
-}
-*/
