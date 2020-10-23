bfb822be518c413ff06dced772fffe6ac0318d2b
hoanmyTran <hoanmytran.hmt@gmail.com>
2020-10-23 09:10:14 +0200

    index modified

--- 02-ionic-core/app.js
+++ 02-ionic-core/app.js
@@ -1,11 +1,18 @@
-const API_URL = `https://devfest-nantes-2018-api.cleverapps.io/blog`;
+/*const API_URL = `https://devfest-nantes-2018-api.cleverapps.io/blog`;
 const IMAGES_URL = `https://devfest2018.gdgnantes.com`;
+
+console.log("Hello");
 
 fetch(API_URL)
     .then(res => res.json())
-    .then(posts => 
+    .then(elements => 
         {
-            const childToPrint = document.getElementById("image");
-            console.log(posts)
-            childToPrint.src = posts[0].title;
+            console.log("hello");
+            let list ="";
+            for (let i in elements) {
+                let element = elements[i];
+                console.log(element);
+            }
         });
+        */
+console.log('haha');
